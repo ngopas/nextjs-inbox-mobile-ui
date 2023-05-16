9bae7983baa774933a17d1ae630f3efe9834d639
Fathur Rohman <fathur.rohman@kemlu.go.id>
2023-05-16 16:39:17 +0200

    responsive example on md

---
+++ src/components/MailItem.tsx
@@ -0,0 +1,18 @@
+const subject = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+const selected = false;
+const MailItem = () => {
+  return (
+    <a className="block border-b-2">
+      <div
+        className={`
+          p-2 md:border-l-2 
+          ${selected ? 'border-blue-400' : 'border-transparent'}
+      `}
+      >
+        <p className="line-clamp-2">{subject}</p>
+      </div>
+    </a>
+  );
+};
+
+export default MailItem;
