b58b96a97278bb10ecc819f320ea7d9e86b09741
Fathur Rohman <fathur.rohman@kemlu.go.id>
2023-05-16 16:23:54 +0200

    create navbar component

--- src/app/(mailbox)/inbox/page.tsx
+++ src/app/(mailbox)/inbox/page.tsx
@@ -1,9 +1,13 @@
+import NavbarMobile from '@/components/mobile/Navbar';
+
 const Inbox = () => {
   return (
     <div className="flex flex-col h-screen">
       <div className="bg-gray-200">top</div>
       <div className="grow">center</div>
-      <div className="bg-gray-300"></div>
+      <footer className="bg-gray-300">
+        <NavbarMobile />
+      </footer>
     </div>
   );
 };
---
+++ src/components/mobile/Navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <div
+      className="
+        bg-gray-200
+        h-16
+      "
+    >
+      <div className="grid grid-cols-2">
+        <div>1</div>
+        <div>2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
