166c9fc3b46728a69228b7f4b34f50fe2f92e0d1
Fathur Rohman <fathur.rohman@kemlu.go.id>
2023-05-16 16:18:49 +0200

    init mobile-ui

---
+++ docs/setup-eslint-prettier-airbnb-base.md
@@ -0,0 +1,5 @@
+Setup eslint prettier airbnb
+
+```sh
+pnpm add eslint-config-airbnb-typescript prettier eslint-plugin-prettier eslint-config-prettier --save-dev
+```
---
+++ src/app/(mailbox)/inbox/page.tsx
@@ -0,0 +1,11 @@
+const Inbox = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="bg-gray-200">top</div>
+      <div className="grow">center</div>
+      <div className="bg-gray-300"></div>
+    </div>
+  );
+};
+
+export default Inbox;
